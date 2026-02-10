5eb00297b9845fc7a348cea7713315a7bd95d1ba
JeanKonan <kon22005@byui.edu>
2026-02-09 23:26:48 -0700

    filtering page fixed

--- src/routes/list.js
+++ src/routes/list.js
@@ -1,8 +1,22 @@
-import { getAllRoutes, getListOfRegions, getListOfSeasons } from '../models/model.js';
+import { getAllRoutes, getListOfRegions, getListOfSeasons, getRoutesByRegion, getRoutesBySeason } from '../models/model.js';
 
 export default async (req, res) => {
+    // console.log("Query Parameters:", req.query);
+    const { region, season } = req.query;
     const regions = await getListOfRegions();
-    const routes = await getAllRoutes();
+    let routes;
+    
+    if (region && season) {
+        routes = await getRoutesByRegion(region);
+        routes = routes.filter(route => route.bestSeason.toLowerCase() === season.toLowerCase());
+    } else if (region) {
+        routes = await getRoutesByRegion(region);
+    } else if (season) {
+        routes = await getRoutesBySeason(season);
+    } else {
+        routes = await getAllRoutes();
+    }
+
     const seasons = await getListOfSeasons();
 
     res.render('routes/list', { 
